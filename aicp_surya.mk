1770e27265ae3840aa224408a7aa021d290a8b04
GtrCraft <nickvanbokhorst@hotmail.com>
2021-05-05 11:59:51 +0200

    surya: Override product info for safetynet
    Change-Id: Ia111e2ac599db6c8b05737983396e9adccdad463

--- aicp_surya.mk
+++ aicp_surya.mk
@@ -31,3 +31,7 @@ PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Fingerprint
+PRODUCT_OVERRIDE_INFO := true
+PRODUCT_OVERRIDE_FINGERPRINT := google/sunfish/sunfish:11/RQ3A.210805.001.A1/7474174:user/release-keys
