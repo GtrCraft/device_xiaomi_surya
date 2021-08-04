c6942c768790b45858b503b128045870cbb057a8
GtrCraft <nickvanbokhorst@hotmail.com>
2021-08-04 15:01:44 +0200

    surya: Add maintainer
    Change-Id: I962bafea928787af0f775161267578bca8febac2

--- aicp_surya.mk
+++ aicp_surya.mk
@@ -35,3 +35,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Fingerprint
 PRODUCT_OVERRIDE_INFO := true
 PRODUCT_OVERRIDE_FINGERPRINT := google/sunfish/sunfish:12/SP2A.220305.012/8177914:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Nick van Bokhorst (GtrCraft)"
