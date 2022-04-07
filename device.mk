631b65b5848d079bbede31949d9c47760580fb4c
GtrCraft <nickvanbokhorst@hotmail.com>
2022-04-07 12:15:06 +0200

    surya: AICP bringup

--- device.mk
+++ device.mk
@@ -331,8 +331,8 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
+    $(LOCAL_PATH)/overlay-aicp
+    
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
