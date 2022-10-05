a0af580fbb794492e67c9f235aebccb04e956ea2
TheStrechh <thestrechh@gmail.com>
2022-10-05 01:42:14 +0000

    surya: kang xiaomiparts from sm8150-common

--- parts/src/org/lineageos/settings/dirac/DiracTileService.java
+++ parts/src/org/lineageos/settings/dirac/DiracTileService.java
@@ -5,29 +5,31 @@ import android.service.quicksettings.TileService;
 
 public class DiracTileService extends TileService {
 
+    private DiracUtils mDiracUtils;
+
     @Override
     public void onStartListening() {
+        mDiracUtils = DiracUtils.getInstance(getApplicationContext());
 
         Tile tile = getQsTile();
-        if (DiracUtils.isDiracEnabled(this)) {
+        if (mDiracUtils.isDiracEnabled()) {
             tile.setState(Tile.STATE_ACTIVE);
         } else {
             tile.setState(Tile.STATE_INACTIVE);
         }
 
         tile.updateTile();
-
         super.onStartListening();
     }
 
     @Override
     public void onClick() {
         Tile tile = getQsTile();
-        if (DiracUtils.isDiracEnabled(this)) {
-            DiracUtils.setMusic(false);
+        if (mDiracUtils.isDiracEnabled()) {
+            mDiracUtils.setEnabled(false);
             tile.setState(Tile.STATE_INACTIVE);
         } else {
-            DiracUtils.setMusic(true);
+            mDiracUtils.setEnabled(true);
             tile.setState(Tile.STATE_ACTIVE);
         }
         tile.updateTile();
